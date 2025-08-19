9d922e64c9d9456ba3283560c9ac5053eb89d963
Kacper Łempicki <kacperlempicki125@wp.pl>
2025-08-19 12:47:58 +0200

    ReportPage ready

---
+++ PetLify/src/components/ui/PetReportCard.jsx
@@ -0,0 +1,21 @@
+const PetReportCard = ({ image, petName, petStatus, reportDate }) => {
+	return (
+		<div className='bg-main rounded-xl overflow-hidden border-cta border-2 min-h-[450px]'>
+			<img src={image} alt='Kicik' className='w-full h-64 object-cover' />
+			<div className='p-4 border-b-1 border-accent space-y-3'>
+				<div className='flex items-center gap-4'>
+					<h2 className='font-bold text-2xl'>{petName}</h2>
+					<p className='bg-negative p-2 rounded-xl'>{petStatus}</p>
+				</div>
+				<p>Zgłoszono: {reportDate}</p>
+			</div>
+			<div className='flex items-center justify-around p-4 gap-4'>
+				<button className='text-cta cursor-pointer p-2'>
+					Zobacz szczegóły
+				</button>
+				<button className='text-cta cursor-pointer p-2'>Edytuj</button>
+			</div>
+		</div>
+	);
+};
+export default PetReportCard;
--- PetLify/src/pages/ReportsPage.jsx
+++ PetLify/src/pages/ReportsPage.jsx
@@ -1,12 +1,70 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import SubPagesNav from '../components/ui/SubPagesNav';
+import BurgerMenu from '../components/ui/BurgerMenu';
+import Gatito from '../img/gatito.jpg';
+import PetReportCard from '../components/ui/PetReportCard';
 const ReportsPage = () => {
 	const location = useLocation();
 	const currentPath = location.pathname;
+	const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 	return (
-		<div>
-			<SubPagesNav currentPath={currentPath} />
-			<div></div>
+		<div className='relative flex'>
+			<SubPagesNav currentPath={currentPath} isBurgerOpen={isBurgerOpen} />
+			<div className='flex flex-col gap-10 px-5 w-full h-screen bg-secondary py-5'>
+				<div className='flex items-center'>
+					<h2 className='text-2xl border-b-2 w-full py-5'>Moje zgłoszenia</h2>
+					<BurgerMenu
+						isBurgerOpen={isBurgerOpen}
+						handleBurger={() => setIsBurgerOpen((prev) => !prev)}
+					/>
+				</div>
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pb-10 h-full custom-scroll pr-2 lg:overflow-y-auto'>
+					{/* Bardzo możliwe, że będziemy zmieniać style reportCardów */}
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+					<PetReportCard
+						image={Gatito}
+						petName={'Kicik'}
+						petStatus={'Zaginiony'}
+						reportDate={'01.03.2024'}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
