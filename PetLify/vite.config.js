1f11a8b17876595b77313b2330fac1d90fb4940a
oswitek <oliwierswitek@gmail.com>
2025-12-09 20:55:32 +0100

    Update vite.config.js

--- PetLify/vite.config.js
+++ PetLify/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname)
+  root: path.resolve(__dirname),
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
