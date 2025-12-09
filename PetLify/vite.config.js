baf3cc53d1e0e0b7a3ceac0d86c24563e07e2c23
oswitek <oliwierswitek@gmail.com>
2025-12-09 20:52:42 +0100

    Move project files to PetLify directory and update Vite config
    Renamed package.json and start.sh to the PetLify directory. Updated vite.config.js to set the root directory, specify the output directory as 'dist', and import 'path' for configuration.

--- PetLify/vite.config.js
+++ PetLify/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+  root: path.resolve(__dirname)
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
 });
